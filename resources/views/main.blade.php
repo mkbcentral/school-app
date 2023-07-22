c06ebf4e1880cb3eed4ef0a8c683ab0cac46bc5e
mkbcentral <mkbcentralcode@gmail.com>
2023-07-22 10:56:49 +0200

    Fix navbar font awosome

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -5,8 +5,8 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>{{ config('app.name', 'MASOMO') }}</title>
-    <link rel="icon" type="image/png" sizes="96x96" href="{{ asset('logo.jpg') }}">
+    <title>{{auth()->user()?auth()->user()->school->name:app_setting('app_name') }}</title>
+    <link rel="icon" type="image/png" sizes="96x96" href="{{ auth()->user()? asset('storage/'.auth()->user()?->school->logo):asset('storage/'.app_setting('app_logo')) }}">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     @livewireStyles
 </head>
