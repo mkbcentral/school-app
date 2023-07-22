9c848cfd78da37e7b6f610ddaf9b3ae1479529e4
mkbcentral <mkbcentralcode@gmail.com>
2023-07-22 10:45:38 +0200

    Fix navbar show images

--- resources/views/layouts/partials/navbar.blade.php
+++ resources/views/layouts/partials/navbar.blade.php
@@ -12,45 +12,32 @@
             @livewire('application.school.scolary-year-widget')
             </div>
         </li>
-        <li class="nav-item dropdown user-menu">
+        <li class="nav-item dropdown user-menu text-white">
             <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">
-
-                @if ( config('app.env')=='production')
-                    <img src="{{ asset('public/defautl-user.jpg') }}"
-                            class="user-image img-circle elevation-2" alt="User Image">
-                @else
-                    <img src="{{ asset('defautl-user.jpg') }}"
-                        class="user-image img-circle elevation-2" alt="User Image">
-
-                @endif
-                <span class="d-none d-md-inline">{{Auth::user()->name}}<span>
+                <img src="{{ asset('defautl-user.jpg') }}" class="user-image img-circle elevation-2" alt="User Image">
+                <span class="d-none d-md-inline text-white">User name<span>
             </a>
             <ul class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
-              <!-- User image -->
-              <li class="user-header bg-primary">
-                    @if (config('app.env')=='production')
-                        <img src="{{ asset('public/defautl-user.jpg') }}"
-                        class="img-circle elevation-2" alt="User Image">
-                    @else
-                        <img src="{{ asset('defautl-user.jpg') }}"
-                        class="img-circle elevation-2" alt="User Image">
-                    @endif
-                <p>
-                  <small>{{Auth::user()->name}}</small>
-                </p>
-              </li>
+                <!-- User image -->
+                <li class="user-header bg-primary">
+                    <img src="{{ asset('defautl-user.jpg') }}" class="img-circle elevation-2" alt="User Image">
+                    <p>
+                        <small>{{Auth::user()->email}}</small>
+                    </p>
+                </li>
 
-              <!-- Menu Footer-->
-              <li class="user-footer">
-                <form method="POST" action="{{ route('logout') }}">
-                    @csrf
+                <!-- Menu Footer-->
+                <li class="user-footer">
+                    <form method="POST" action="{{ route('logout') }}">
+                        @csrf
                         <a href="{{ route('logout') }}"
-                        onclick="event.preventDefault();
-                        this.closest('form').submit();" class="btn btn-default btn-flat float-right">Déconnexion</a>
-                </form>
+                           onclick="event.preventDefault();
+                        this.closest('form').submit();"
+                           class="btn btn-default btn-flat float-right">Déconnexion</a>
+                    </form>
 
-              </li>
+                </li>
             </ul>
-          </li>
+        </li>
     </ul>
 </nav>
